2c597400f3a575080c4f7c88a90581426edc2a7f
jinhan1414 <jinhan1414@gmail.com>
2018-08-04 23:13:05 +0800

    开始进行ci集成测试

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 test
+sqlite jpa
+https://www.cnblogs.com/zj0208/p/5850345.html
---
+++ src/main/java/me/jincs/blog/controller/HomePageController.java
@@ -0,0 +1,16 @@
+package me.jincs.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping({"/index", "/"})
+    public String index() {
+
+
+        return "homePage/index";
+    }
+}
