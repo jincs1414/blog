4277801fe2e019503f807dbf98b248f57716c1bf
jinhan1414 <jinhan1414@gmail.com>
2018-08-08 22:03:45 +0800

    thymeleaf的cdn配置功能完成 加入dev-tools 进行开发 接下来完成数据的采集功能

--- src/main/java/me/jincs/blog/configure/ThymleafConfig.java
+++ src/main/java/me/jincs/blog/configure/ThymleafConfig.java
@@ -3,24 +3,13 @@ package me.jincs.blog.configure;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.context.IExpressionContext;
-import org.thymeleaf.context.IWebContext;
-import org.thymeleaf.exceptions.TemplateProcessingException;
-import org.thymeleaf.linkbuilder.AbstractLinkBuilder;
 import org.thymeleaf.linkbuilder.ILinkBuilder;
 import org.thymeleaf.linkbuilder.StandardLinkBuilder;
 import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.util.Validate;
-import org.unbescape.uri.UriEscape;
 
-import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.*;
+import java.util.Map;
 
 @Configuration
 public class ThymleafConfig {
@@ -34,7 +23,7 @@ public class ThymleafConfig {
 
     public class CdnLinkBuilder implements ILinkBuilder {
         private ILinkBuilder standardLinkBuilder = new StandardLinkBuilder();
-
+        private String[] suffixArray = {".js", ".css"};
         @Override
         public String getName() {
             return "cdn builder";
@@ -42,15 +31,24 @@ public class ThymleafConfig {
 
         @Override
         public Integer getOrder() {
-            return 2;
+            return 1;
         }
 
         @Override
         public String buildLink(IExpressionContext iExpressionContext, String s, Map<String, Object> map) {
-            if (!StringUtils.isEmpty(cdnPrefix) && (s.endsWith(".js") || s.endsWith(".css"))) {
+            if (!StringUtils.isEmpty(cdnPrefix) && isHandingType(s)) {
                 return cdnPrefix + s;
             }
             return standardLinkBuilder.buildLink(iExpressionContext, s, map);
+        }
+
+        private boolean isHandingType(String s) {
+            for (String suffix : suffixArray) {
+                if (s.endsWith(suffix)) {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
